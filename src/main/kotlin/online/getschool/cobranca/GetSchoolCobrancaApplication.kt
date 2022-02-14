28548c1e2b97dc009a6285bd43dfbbaeebab5553
Rodolfo Menardi <rodolfomenardi@gmail.com>
2022-02-14 14:12:10 -0300

    Projeto spring inicial

---
+++ src/main/kotlin/online/getschool/cobranca/GetSchoolCobrancaApplication.kt
@@ -0,0 +1,11 @@
+package online.getschool.cobranca
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GetSchoolCobrancaApplication
+
+fun main(args: Array<String>) {
+	runApplication<GetSchoolCobrancaApplication>(*args)
+}
---
+++ src/test/kotlin/online/getschool/cobranca/GetSchoolCobrancaApplicationTests.kt
@@ -0,0 +1,13 @@
+package online.getschool.cobranca
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GetSchoolCobrancaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
